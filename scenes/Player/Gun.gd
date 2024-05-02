c9576c95fca4a79678cdc8de3807c51ef27420bb
Kirill Platonov <platonovk1998@ya.ru>
2024-05-02 18:41:20 +0500

    fix

--- scenes/Player/Gun.gd
+++ scenes/Player/Gun.gd
@@ -21,7 +21,6 @@ func shoot():
 	
 	raycast.force_raycast_update()
 	var collider = raycast.get_collider()
-	print(collider)
 	if !collider: return
 	if collider.is_in_group("enemy"):
 		collider.queue_free()
