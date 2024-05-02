f2edb42267e9803328c845812440da64a28e77cc
Kirill Platonov <platonovk1998@ya.ru>
2024-05-02 15:33:10 +0500

    input handler + movement

---
+++ scenes/Player/InputHandler.gd
@@ -0,0 +1,46 @@
+extends Node
+
+
+signal movement_inputted(input_vector)
+signal jump_pressed
+signal escape_pressed
+signal mouse_moved(input_vector)
+signal main_fire_pressed
+signal main_fire_released
+signal alt_fire_pressed
+signal alt_fire_released
+
+
+func _process(_delta):
+	var movement_input_vector = Vector2.ZERO
+	if Input.is_action_pressed("move_forward"):
+		movement_input_vector.y += 1
+	if Input.is_action_pressed("move_back"):
+		movement_input_vector.y -= 1
+	if Input.is_action_pressed("move_right"):
+		movement_input_vector.x += 1
+	if Input.is_action_pressed("move_left"):
+		movement_input_vector.x -= 1
+	movement_input_vector = movement_input_vector.normalized()
+	movement_inputted.emit(movement_input_vector)
+	
+	if Input.is_action_just_pressed("jump"):
+		jump_pressed.emit()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		escape_pressed.emit()
+	
+	if Input.is_action_just_pressed("main_fire"):
+		main_fire_pressed.emit()
+	if Input.is_action_just_released("main_fire"):
+		main_fire_released.emit()
+	if Input.is_action_just_pressed("alt_fire"):
+		alt_fire_pressed.emit()
+	if Input.is_action_just_released("alt_fire"):
+		alt_fire_released.emit()
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if event.relative == Vector2.ZERO: return
+		mouse_moved.emit(event.relative)
---
+++ scenes/Player/player.gd
@@ -0,0 +1,70 @@
+extends CharacterBody3D
+
+
+@onready var input_handler = $InputHandler
+@onready var camera = $Camera3D
+var gravity_acceleration = 9.8
+var player_move_speed = 5
+var jump_speed = 5
+var camera_sensitivity = 0.1
+
+
+func _ready():
+	input_handler.movement_inputted.connect(_on_input_handler_movement_inputted)
+	input_handler.jump_pressed.connect(_on_input_handler_jump_pressed)
+	input_handler.escape_pressed.connect(_on_input_handler_escape_pressed)
+	input_handler.mouse_moved.connect(_on_input_handler_mouse_moved)
+	input_handler.main_fire_pressed.connect(_on_input_handler_main_fire_pressed)
+	input_handler.main_fire_released.connect(_on_input_handler_main_fire_released)
+	input_handler.alt_fire_pressed.connect(_on_input_handler_alt_fire_pressed)
+	input_handler.alt_fire_released.connect(_on_input_handler_alt_fire_released)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _process(delta):
+	velocity.y -= gravity_acceleration * delta
+	
+	move_and_slide()
+
+
+func _on_input_handler_movement_inputted(input_vector: Vector2):
+	input_vector = input_vector.rotated(get_rotation().y)
+	input_vector = input_vector * player_move_speed
+	velocity.x = input_vector.x
+	velocity.z = -input_vector.y
+
+
+func _on_input_handler_jump_pressed():
+	if is_on_floor():
+		velocity.y = jump_speed
+
+
+func _on_input_handler_escape_pressed():
+	get_tree().quit()
+
+
+func _on_input_handler_mouse_moved(input_vector):
+	input_vector *= camera_sensitivity
+	rotate_y(deg_to_rad(-input_vector.x))
+	
+	var new_camera_rotation = camera.get_rotation()
+	new_camera_rotation.x = (new_camera_rotation.x - deg_to_rad(input_vector.y))
+	new_camera_rotation.x = clampf(new_camera_rotation.x, -PI/2, PI/2)
+	camera.set_rotation(new_camera_rotation)
+
+
+func _on_input_handler_main_fire_pressed():
+	print("main fire pressed")
+
+
+func _on_input_handler_main_fire_released():
+	print("main fire released")
+
+
+func _on_input_handler_alt_fire_pressed():
+	print("alt fire pressed")
+
+
+func _on_input_handler_alt_fire_released():
+	print("alt fire released")
