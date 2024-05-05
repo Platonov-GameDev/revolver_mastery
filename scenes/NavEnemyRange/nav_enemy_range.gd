b0fae455ba9a1b9fb951e3c7425782aaf2f25247
Kirill Platonov <platonovk1998@ya.ru>
2024-05-05 17:11:14 +0500

    fix range enemy aiming

--- scenes/NavEnemyRange/nav_enemy_range.gd
+++ scenes/NavEnemyRange/nav_enemy_range.gd
@@ -22,7 +22,24 @@ func _physics_process(delta):
 	velocity.y -= Global.gravity_acceleration * delta
 	
 	if is_on_floor():
-		if position.distance_to(player.position) <= 20:
+		var raycast_to_player = RayCast3D.new()
+		raycast_to_player.position = muzzle.global_position
+		get_parent().add_child(raycast_to_player)
+		var player_center_position = player.global_position
+		player_center_position.y += 0.5
+		var player_direction = (player_center_position - raycast_to_player.global_position).normalized()
+		raycast_to_player.target_position = player_direction * 20
+		raycast_to_player.set_collision_mask_value(3, true)
+		raycast_to_player.force_raycast_update()
+		var raycast_collider = raycast_to_player.get_collider()
+		raycast_to_player.queue_free()
+		
+		var player_visible = false
+		if is_instance_valid(raycast_collider):
+			if raycast_collider.is_in_group("player"):
+				player_visible = true
+		
+		if player_visible:
 			if !is_reloading:
 				var projectile = projectile_scene.instantiate()
 				projectile.position = muzzle.global_position
