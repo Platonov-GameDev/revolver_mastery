46ad268f497496e86e652fb5f54413a0fd1c898f
Kirill Platonov <platonovk1998@ya.ru>
2024-05-07 18:49:44 +0500

    basic fire

--- scenes/ComboGun/combo_gun.gd
+++ scenes/ComboGun/combo_gun.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
 
+# GENERAL
+@export var camera: Camera3D
+@export var trail_scene: PackedScene
 @onready var down_timer = $DownTimer
+enum State {IDLE}
+var current_state = State.IDLE
+
+# DASH
 var last_movement_time = 0
 var last_movement_direction
 var dash_move_tap_window = .1
@@ -9,8 +16,6 @@ var is_dash_move_done = false
 var is_dash_shoot_done = false
 var dash_prepared_time = 0
 var dash_preparation_window = .1
-enum State {IDLE, DASHING}
-var current_state = State.IDLE
 
 
 func _ready():
@@ -22,11 +27,13 @@ func _process(_delta):
 
 
 func movement_pressed(direction):
+	if get_parent().current_movement_state == PlayerMovementState.DASHING: return
 	last_movement_direction = direction
 	last_movement_time = Time.get_unix_time_from_system()
 
 
 func movement_released(direction):
+	if get_parent().current_movement_state == PlayerMovementState.DASHING: return
 	if direction != last_movement_direction: return
 	var current_time = Time.get_unix_time_from_system()
 	var time_since_movement_press = current_time - last_movement_time
@@ -47,20 +54,41 @@ func process_dash():
 
 
 func dash():
-	if current_state != State.IDLE: return
 	is_dash_move_done = false
 	is_dash_shoot_done = false
 	get_parent().dash(last_movement_direction)
-	current_state = State.DASHING
-	down_timer.start()
 
 
 func fire_pressed():
+	if current_state != State.IDLE: return
+	
 	if is_dash_move_done:
 		dash()
 	else:
 		dash_prepared_time = Time.get_unix_time_from_system()
 		is_dash_shoot_done = true
+	
+	var raycast = RayCast3D.new()
+	raycast.position = global_position
+	raycast.rotation = camera.global_rotation
+	raycast.target_position = Vector3(0, 0, -100)
+	raycast.set_collision_mask_value(1, true)
+	raycast.set_collision_mask_value(2, true)
+	get_parent().get_parent().add_child(raycast)
+	
+	raycast.force_raycast_update()
+	var collider = raycast.get_collider()
+	if collider:
+		if collider.is_in_group("enemy"):
+			collider.receive_damage(30)
+	
+	var shot_trail = trail_scene.instantiate()
+	shot_trail.scale.z = raycast.position.distance_to(raycast.get_collision_point()) / 100
+	shot_trail.position = global_position
+	shot_trail.rotation = camera.global_rotation
+	get_parent().get_parent().add_child(shot_trail)
+	
+	raycast.queue_free()
 
 
 func fire_released():
