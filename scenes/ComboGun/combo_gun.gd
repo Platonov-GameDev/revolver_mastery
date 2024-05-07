40b5eeca370cefc9160fd22c5a5a850731beda69
Kirill Platonov <platonovk1998@ya.ru>
2024-05-08 00:33:45 +0500

    auto

--- scenes/ComboGun/combo_gun.gd
+++ scenes/ComboGun/combo_gun.gd
@@ -5,7 +5,9 @@ extends Node3D
 @export var camera: Camera3D
 @export var trail_scene: PackedScene
 @onready var down_timer = $DownTimer
-enum State {IDLE, DOWN}
+@onready var auto_windup_timer = $AutoWindupTimer
+@onready var auto_shoot_timer = $AutoShootTimer
+enum State {IDLE, DOWN, FIRE, AUTO}
 var current_state = State.IDLE
 
 # DASH
@@ -17,9 +19,14 @@ var is_dash_shoot_done = false
 var dash_prepared_time = 0
 var dash_preparation_window = .1
 
+# AUTO
+
+
 
 func _ready():
 	down_timer.timeout.connect(_on_down_timer_timeout)
+	auto_windup_timer.timeout.connect(_on_auto_windup_timer_timeout)
+	auto_shoot_timer.timeout.connect(_on_auto_shoot_timer_timeout)
 
 
 func _process(_delta):
@@ -68,6 +75,14 @@ func fire_pressed():
 		dash_prepared_time = Time.get_unix_time_from_system()
 		is_dash_shoot_done = true
 	
+	shoot_ray(30)
+	
+	auto_windup_timer.start()
+	
+	current_state = State.FIRE
+
+
+func shoot_ray(damage_amount: int):
 	var raycast = RayCast3D.new()
 	raycast.position = global_position
 	raycast.rotation = camera.global_rotation
@@ -80,7 +95,7 @@ func fire_pressed():
 	var collider = raycast.get_collider()
 	if collider:
 		if collider.is_in_group("enemy"):
-			collider.receive_damage(30)
+			collider.receive_damage(damage_amount)
 	
 	var shot_trail = trail_scene.instantiate()
 	shot_trail.scale.z = raycast.position.distance_to(raycast.get_collision_point()) / 100
@@ -89,13 +104,15 @@ func fire_pressed():
 	get_parent().get_parent().add_child(shot_trail)
 	
 	raycast.queue_free()
-	
-	current_state = State.DOWN
-	down_timer.start()
 
 
 func fire_released():
-	pass
+	auto_windup_timer.stop()
+	auto_shoot_timer.stop()
+	
+	if current_state != State.DOWN && current_state != State.IDLE:
+		current_state = State.DOWN
+		down_timer.start()
 
 
 func alt_fire_pressed():
@@ -108,3 +125,12 @@ func alt_fire_released():
 
 func _on_down_timer_timeout():
 	current_state = State.IDLE
+
+
+func _on_auto_windup_timer_timeout():
+	current_state = State.AUTO
+	auto_shoot_timer.start()
+
+
+func _on_auto_shoot_timer_timeout():
+	shoot_ray(10)
